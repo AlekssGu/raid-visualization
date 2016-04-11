d1f019137267a8fc8db239204b60a71bff942360
AlekssGu <gusevs.aleksandrs@gmail.com>
2016-04-11 22:35:31 +0300

    Added images and navigation

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,35 @@ $(document).ready(function() {
   $('.slider').slick({
     centerMode: true,
     centerPadding: '60px',
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true
+  });
+
+  function change_image_source(mode,level) {
+    var image = $('#raid-gif');
+    var src;
+
+    if(mode !== 'WRITE' && mode !== 'READ' && mode !== 'RECOVER') {
+      return;
+    }
+
+    src = 'images/' + level + '_' + mode + '.gif';
+    image.attr('src',src);
+    image.removeClass('hidden');
+  };
+
+  $('.item a').click(function(e){
+    var mode = $(this).text();
+    var level = $(this).attr('level').toUpperCase();
+
+    if(mode.trim() && level.trim()) {
+      change_image_source(mode,level);
+    }
+
+    e.preventDefault();
   });
 });
